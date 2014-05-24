188f1b873b8017d1332c20841df181f9146dadd1
svankmajer <syrii@msn.com>
2014-05-23 21:26:30 -0300

    Add first spec for Experience.
    This commit has emotional value only. I want to do this right. I will
    keep testing properly only to get to the highest condition of testing expertise.

---
+++ spec/models/experience_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Experience do
+  describe '.approved' do
+    it 'shows only approved experiences' do
+      good_experience = Experience.create(
+        pseudonym: 'svankmajer',
+        title: 'compartmentalization',
+        body: 'not quite long ago...',
+        is_approved: true
+      )
+
+      bad_experience = Experience.create(
+        pseudonym: 'rejamknavs',
+        title: 'train wreck',
+        body: 'this is not going to be fun...'
+      )
+
+      experiences = Experience.approved
+
+      experiences.should == [good_experience]
+    end
+  end
+end
