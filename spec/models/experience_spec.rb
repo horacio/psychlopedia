5a668a51ad9240407a2daf705fd2193a37ffbeb5
svankmajer <syrii@msn.com>
2014-06-24 14:31:15 -0300

    Add specs for validations and associations.

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe Experience do
-  describe Experience, 'validations' do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :body }
-    it { should validate_presence_of :pseudonym }
-  end
+describe Experience, 'validations' do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :pseudonym }
+end
 
+describe Experience, 'scopes' do
   describe '.approved' do
     it 'returns an array of approved experiences' do
       approved_experience = create(:experience, is_approved: true)
@@ -19,3 +19,8 @@ describe Experience do
   end
 end
 
+describe Experience, 'associations' do
+  it { should have_many(:categories).through(:experience_categories) }
+  it { should have_many(:cocktails) }
+  it { should accept_nested_attributes_for(:cocktails) }
+end
