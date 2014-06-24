81c80fe779e6a5d796191bebd0cb70fd42d0e5ad
svankmajer <syrii@msn.com>
2014-06-23 22:03:37 -0300

    Fix based on Hound comments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.order = 'random'
   config.include ExperiencesHelper
 end
